1fc2490fe8aa816e78011339d8e7fc7233c9ca09
Baz Murphy <bazmurphy@gmail.com>
2024-05-25 15:18:40 +0100

    initial commit

---
+++ consumer/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IBM/sarama"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatalf("failed to start consumer: %v", err)
+	}
+
+	topic := "my_topic"
+	var partition int32 = 0
+	offset := sarama.OffsetNewest
+
+	partitionConsumer, err := consumer.ConsumePartition(topic, partition, offset)
+	if err != nil {
+		log.Fatalf("failed to start partition consumer: %v", err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	var consumerMessageCount, consumerErrorCount int
+
+	go func() {
+		log.Println("consumer loop started...")
+		defer func() {
+			log.Println("consumer loop ended...")
+		}()
+
+		for {
+			select {
+			case message := <-partitionConsumer.Messages():
+				log.Println("consumed message:")
+				log.Println("\ttopic:", message.Topic)
+				log.Println("\toffset:", message.Offset)
+				log.Println("\tpartition:", message.Partition)
+				log.Println("\tkey:", string(message.Key))
+				log.Println("\tvalue:", string(message.Value))
+				consumerMessageCount++
+			case err := <-partitionConsumer.Errors():
+				log.Println("error consuming message:", err)
+				consumerErrorCount++
+			case <-signals:
+				// AsyncClose initiates a shutdown of the PartitionConsumer.
+				// This method will return immediately, after which you should continue to service the 'Messages' and 'Errors' channels until they are empty.
+				// It is required to call this function, or Close before a consumer object passes out of scope, as it will otherwise leak memory.
+				// You must call this before calling Close on the underlying client.
+				partitionConsumer.AsyncClose()
+				return
+			}
+		}
+	}()
+
+	<-signals
+
+	// Close shuts down the consumer.
+	// It must be called after all child PartitionConsumers have already been closed.
+	consumer.Close()
+
+	log.Println("metrics:")
+	log.Println("\tconsumer message count:", consumerMessageCount)
+	log.Println("\tconsumer error count:", consumerErrorCount)
+}
---
+++ producer/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/google/uuid"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatalf("failed to start producer: %v", err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	var producerEnqueuedCount, producerSuccessCount, producerErrorCount int
+
+	go func() {
+		log.Println("producer loop started...")
+		defer func() {
+			log.Println("producer loop ended...")
+		}()
+
+		for {
+			newMessageKey := uuid.NewString()
+			newMessageValue := "testing 123"
+
+			newMessage := &sarama.ProducerMessage{
+				Topic: "my_topic",
+				Key:   sarama.StringEncoder(newMessageKey),
+				Value: sarama.StringEncoder(newMessageValue),
+			}
+
+			select {
+			case producer.Input() <- newMessage:
+				producerEnqueuedCount++
+			case message := <-producer.Successes():
+				log.Println("success producing message:")
+				log.Println("\ttopic:", message.Topic)
+				log.Println("\toffset:", message.Offset)
+				log.Println("\tpartition:", message.Partition)
+				log.Println("\tkey:", message.Key)
+				log.Println("\tvalue:", message.Value)
+				producerSuccessCount++
+			case err := <-producer.Errors():
+				log.Println("error producing message:", err)
+				producerErrorCount++
+			case <-signals:
+				// AsyncClose triggers a shutdown of the producer.
+				// The shutdown has completed when both the Errors and Successes channels have been closed.
+				// When calling AsyncClose, you *must* continue to read from those channels in order to drain the results of any messages in flight.
+				producer.AsyncClose()
+				return
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	<-signals
+
+	// Close shuts down the producer and waits for any buffered messages to be flushed.
+	// You must call this function before a producer object passes out of scope, as it may otherwise leak memory.
+	// You must call this before process shutting down, or you may lose messages.
+	// You must call this before calling Close on the underlying client.
+	producer.Close()
+
+	log.Println("metrics:")
+	log.Println("\tproducer enqueued count:", producerEnqueuedCount)
+	log.Println("\tproducer success count:", producerSuccessCount)
+	log.Println("\tproducer error count:", producerErrorCount)
+}
